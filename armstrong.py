c5507214bcdeb53eec490881f75c124b4be42077
anaghatambe05 <anaghatambe05@gmail.com>
2026-07-26 20:53:35 +0530

    Fixed error in line 7

--- armstrong.py
+++ armstrong.py
@@ -4,7 +4,7 @@ sum=0
 
 while temp > 0:
     digit = temp % 10
-    sum = sum+digit*len(str(num))   # Error:use ** instead of *
+    sum = sum + digit ** len(str(num)) 
     temp = temp//10
 
 if sum == num:
