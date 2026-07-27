97b4fd3e44a1636bea7a50c82a56bda2843e4290
VEER2409 <vireshshankad07@gmail.com>
2026-07-27 14:06:42 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -5,7 +5,7 @@ text = input("Enter a word or number: ").strip()
 cleaned_text = text.lower()
 
 # Check if the string is equal to its reverse
-if cleaned_text == cleaned_text[::+1]:
+if cleaned_text == cleaned_text[::-1]:
     print(f"Result: '{text}' IS a palindrome! \n")
 else:
     print(f"Result: '{text}' is NOT a palindrome.\n")
